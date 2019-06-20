d6575c425776f119888de153be34d75ecf2c4c79
novinary <novina.dev@gmail.com>
2019-06-20 17:01:36 +0100

    Graph class in progress

--- graph_adventure/graphs.py
+++ graph_adventure/graphs.py
@@ -35,3 +35,14 @@ newStack.pop()
 
 print(newStack.stack)
 
+class Graph():
+    def __init__(self):
+        self.vertices = {}
+    def add_vertices(self, vertex):
+        self.vertices[vertex] = set()
+    def add_edges(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("That vertex does not exist!")
