f0a1cd38f8d40eacb88eb7b3df5c68ee57ae1d00
novinary <novina.dev@gmail.com>
2019-06-21 09:52:52 +0100

    Used stack to get a graph and small traversal loop working

--- graph_adventure/graphs.py
+++ graph_adventure/graphs.py
@@ -46,3 +46,37 @@ class Graph():
             self.vertices[v2].add(v1)
         else:
             raise IndexError("That vertex does not exist!")
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+    def dft_r(self, start_vert, visited=None):
+        if visited is None:
+            visited = set()
+        visited.add(start_vert)
+        print(start_vert)
+        for child_vert in self.vertices[start_vert]:
+            if child_vert not in visited:
+                self.dft_r(child_vert, visited)
+    def dft(self, starting_vertex_id):
+        stack = Stack()
+        stack.push(starting_vertex_id)
+        visited = set()
+        while stack.size() > 0:
+            v = stack.pop()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+                for next_vert in self.vertices[v]:
+                    stack.push(next_vert)
+    
+    
+
+newGraph = Graph()
+newGraph.add_vertices(1)
+newGraph.add_vertices(2)
+newGraph.add_edges(1,2)
+newGraph.add_directed_edge(1,2)
+
+print(newGraph.vertices)
